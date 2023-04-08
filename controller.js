749dfc07d916c90f23d950820afb9240ca6d1717
Raihan Afdal Hadi <raihanafdlhdi@gmail.com>
2023-04-08 10:38:18 +0700

    add query by id

--- controller.js
+++ controller.js
@@ -17,3 +17,17 @@ exports.tampil = function(req, res) {
     }
   });
 };
+
+// menampilkan semua dtaa mahasiswa berdasarkan id
+
+exports.tampilById = function(req, res){
+  let id = req.params.id;
+  connection.query('SELECT * FROM mahasiswa where id_mahasiswa = ?', [id],
+  function(error, rows, field){
+    if(error){
+      console.log(error);
+    } else {
+      response.ok(rows, res)
+    }
+  })
+};
